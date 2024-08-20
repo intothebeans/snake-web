0b3f7f4c9d8afcea94616fff10f77a17562293c0
Aiden <88350376+intothebeans@users.noreply.github.com>
2024-08-20 01:40:57 -0400

    add movement and refactor

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 const game_border_color = "#9399b2";
 const game_background_color = "#11111b";
+const snake_color = "#a6e3a1";
+const snake_border_color = "#40a02b";
 
 var game_canvas = document.getElementById("gameCanvas");
 var ctx = game_canvas.getContext("2d");
@@ -12,20 +14,38 @@ let snake = [
     { x: 120, y: 150 },
     { x: 110, y: 150 },
 ];
-ctx.fillStyle = game_background_color;
-ctx.strokeStyle = game_background_color;
 
-ctx.fillRect(0, 0, game_canvas.width, game_canvas.height);
-ctx.strokeStyle(0, 0, gameCanvas.width, game_canvas.height);
+let dx = 10;
+let dy = 0;
+clearCanvas();
+
+drawSnake();
+slither();
 
 function drawSnakeSegment(snakeSegment) {
-    ctx.fillStyle = "#a6e3a1";
-    ctx.strokeStyle = "#40a02b";
+    ctx.fillStyle = snake_color;
+    ctx.strokeStyle = snake_border_color;
 
     ctx.fillRect(snakeSegment.x, snakeSegment.y, 10, 10);
     ctx.strokeRect(snakeSegment.x, snakeSegment.y, 10, 10);
 }
 
 function drawSnake() {
-    snake.forEach(drawSnakeSegment());
+    snake.forEach(drawSnakeSegment);
+}
+
+function slither() {
+    const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+    snake.unshift(head);
+    snake.pop();
+
+}
+
+function clearCanvas() {
+    ctx.fillStyle = game_background_color;
+    ctx.strokeStyle = game_border_color;
+
+    ctx.fillRect(0, 0, game_canvas.width, game_canvas.height);
+    ctx.strokeRect(0, 0, game_canvas.width, game_canvas.height);
+
 }
