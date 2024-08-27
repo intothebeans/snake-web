7dbcbd9059eefb018fbaad57c0c0f976a684b7ff
Aiden <88350376+intothebeans@users.noreply.github.com>
2024-08-27 19:02:31 -0400

    add user input and collisions

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ let dx = 10;
 let dy = 0;
 clearCanvas();
 
-drawSnake();
-slither();
+main();
+document.addEventListener("keydown", userInput);
 
 function drawSnakeSegment(snakeSegment) {
     ctx.fillStyle = snake_color;
@@ -48,4 +48,57 @@ function clearCanvas() {
     ctx.fillRect(0, 0, game_canvas.width, game_canvas.height);
     ctx.strokeRect(0, 0, game_canvas.width, game_canvas.height);
 
+}
+
+function userInput(event) {
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+
+    const key_pressed = event.keyCode;
+    const up = dy === -10;
+    const down = dy === 10;
+    const right = dx === 10;
+    const left = dx === -10;
+
+    if (key_pressed == LEFT_KEY && !right) {
+        dx = -10;
+        dy = 0;
+    }
+
+    if (key_pressed == RIGHT_KEY && !left) {
+        dx = 10;
+        dy = 0;
+    }
+    if (key_pressed == DOWN_KEY && !up) {
+        dx = 0;
+        dy = 10;
+    }
+    if (key_pressed == UP_KEY && !down) {
+        dx = 0;
+        dy = -10;
+    }
+}
+
+function checkCollision() {
+    // self collision 
+    for (let i = 4; i < snake.length; i++) {
+        if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+            return true
+        }
+    }
+    // border collision 
+    return snake[0].x <= 0 || snake[0].x + 10 >= game_canvas.width || snake[0].y <= 0 ||
+        snake[0].y + 10 >= game_canvas.height;
+}
+
+function main() {
+    setTimeout(function onTick() {
+        clearCanvas();
+        slither();
+        drawSnake();
+        if (checkCollision()) { return }
+        main();
+    }, 100);
 }
